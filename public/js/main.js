1f796796dd8f6a050ecfa0d18acc162588ed6659
Gabriel Ramos de Sousa <gabrielramos.email@gmail.com>
2020-03-15 10:47:36 -0300

    ajax request and changing phrase

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,7 @@ $(function () {
     inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
     $("#botao-placar").click(mostraPlacar);
+    $("#botao-frase").click(fraseAleatoria);
 });
 
 function atualizaTamanhoFrase() {
@@ -31,8 +32,8 @@ function inicializaContadores() {
 }
 
 function inicializaMarcadores() {
-    var frase = $(".frase").text();
     campo.on("input", function () {
+        var frase = $(".frase").text();
         var digitado = campo.val();
         var comparavel = frase.substr(0, digitado.length);
 
@@ -47,8 +48,8 @@ function inicializaMarcadores() {
 }
 
 function inicializaCronometro() {
-    var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function () {
+        var tempoRestante = $("#tempo-digitacao").text();
         var cronometroID = setInterval(function () {
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
@@ -76,4 +77,9 @@ function reiniciaJogo() {
     campo.toggleClass("campo-desativado");
     campo.removeClass("borda-vermelha");
     campo.removeClass("borda-verde");
+}
+
+function atualizaTempoInicial(tempo) {
+    tempoInicial = tempo;
+    $("#tempo-digitacao").text(tempo);
 }
